08a229e0f36bcd20398eb9d526fa198ec7b26545
adebayo-moses <adebayomoses597@gmail.com>
2023-07-15 13:17:23 +0100

    loader and barebone setup

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
 // import "./style.css";
+import gsap from "gsap";
 import "./scss/style.scss";
+
+// loader
+const loadingOverlay = document.querySelector(".loading-overlay");
+
+if (loadingOverlay) {
+  const tl = gsap
+    .timeline()
+    .to(loadingOverlay, {
+      "--percentage": "'100%'",
+      roundProps: 1,
+      duration: 2,
+    })
+    .to(loadingOverlay, {
+      clipPath: "inset(0 0 100% 0)",
+      pointerEvents: "none",
+      duration: 0.4,
+    })
+    .from(".page-header h1", {
+      opacity: 0,
+      y: 100,
+    });
+}
